e50dede91928db5a285d02dad01b0e88f271660f
Alex <alex.muharovskiy@avionos.com>
2022-08-25 16:43:13 +0200

    add styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {theme} from "themes";
+import {ThemeProvider} from "styled-components";
 import {appWithTranslation} from "next-i18next";
 
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>)
 }
 
 export default appWithTranslation(MyApp)
