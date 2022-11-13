c9280ed0f82b73c640adfe7ef2cceec638f01cff
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-13 17:49:17 +0100

    add scss

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
-const { i18n } = require("./next-i18next.config");
+const {i18n} = require("./next-i18next.config");
+const path = require('path')
 
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n,
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true
-  }
+    i18n,
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        styledComponents: true
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
 }
 
 module.exports = nextConfig
