5ce0b3a45fd44299ef291847c5a6279908cd47e9
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-13 12:17:40 +0100

    add default styles.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type {AppProps} from 'next/app'
 import {theme} from "themes";
 import {ThemeProvider} from "styled-components";
 import {appWithTranslation} from "next-i18next";
+import './styles.css'
 
 
 function MyApp({Component, pageProps}: AppProps) {
