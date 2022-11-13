8693fa8971dc7d2128be6b8000ef0f721ef5b2cd
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-13 17:54:12 +0100

    add theme

--- themes/index.tsx
+++ themes/index.tsx
@@ -1,1 +1,19 @@
-export const theme = {}
+import {MainLayoutStyles} from "@components/layout/MainLayout";
+import { DefaultTheme } from 'styled-components'
+
+
+export interface Theme {
+    mainLayout: MainLayoutStyles
+}
+
+export const theme: Theme = {
+    mainLayout: {
+        footerStyle: {
+            socialIconStyle: {
+                size: 32,
+                color: ["#C3D0D5"],
+                hoverColor: "#F5BE17",
+            }
+        }
+    }
+}
