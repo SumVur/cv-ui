7f5bc6fb3ce13ceb75f9f0e4fe27aced3b545308
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-13 17:53:13 +0100

    add social links to footer

--- components/common/organisms/Footer/index.tsx
+++ components/common/organisms/Footer/index.tsx
@@ -1,18 +1,27 @@
 import {FC} from "react";
+import {SocialIconStyle} from "@common/atoms";
+import {SocialLinks} from "@common/molecules";
+import useSWR from "swr";
+import fetch from "node-fetch";
 
 export interface FooterStyle {
-
+    socialIconStyle: SocialIconStyle
 }
 
 export interface FooterInterface {
-
 }
 
 type FooterProps = FooterInterface & FooterStyle
-const Footer:FC<FooterProps> = ({}) => {
-    return(
+const Footer: FC<FooterProps> = ({socialIconStyle,}) => {
+    const {data, error} = useSWR('/api/social-links', (apiURL: string) =>fetch(apiURL,{timeout:8000}).then(res => res.json()))
+
+    return (
         <div>
-            Footer
+            <SocialLinks
+                paraphrase={"Footer"}
+                socialLinksItems={data}
+                socialIconStyle={socialIconStyle}
+            />
         </div>
     )
 }
