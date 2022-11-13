3d9c18a4aee2e1e25490b55a0a708dc3d864d4fa
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-14 00:51:18 +0100

    add font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,27 @@ import type {AppProps} from 'next/app'
 import {theme} from "themes";
 import {ThemeProvider} from "styled-components";
 import {appWithTranslation} from "next-i18next";
-import './styles.css'
+import './styles.scss'
 import {MainLayout} from "@layout";
+import {Cardo} from '@next/font/google'
 
+const cardo = Cardo({weight: "400"})
 
 function MyApp({Component, pageProps}: AppProps) {
     return (
-        <ThemeProvider theme={theme}>
-            <MainLayout>
-                <Component {...pageProps} />
-            </MainLayout>
-        </ThemeProvider>
+        <>
+            <style jsx global>{`
+              html {
+                font-family: ${cardo.style.fontFamily};
+              }
+            `}</style>
+
+            <ThemeProvider theme={theme}>
+                <MainLayout>
+                    <Component {...pageProps} />
+                </MainLayout>
+            </ThemeProvider>
+        </>
     )
 }
 
