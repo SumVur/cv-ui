e9bc6b3aeb7515965032305b51aff1e5431d760c
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-13 13:03:56 +0100

    add profile-image

---
+++ components/common/atoms/ProfileImage/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const ProfileImage = ({}) => {
+    return (
+        <Image
+            src="/profile-image.png"
+            alt={"profile-image"}
+            style={{borderRadius: "50%"}}
+            width={152}
+            height={152}
+        />
+    )
+}
+
+export default ProfileImage;
