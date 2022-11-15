6875b8ad17543cb934659c51f3e5d6f12e98bb5e
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-15 21:25:06 +0100

    FlexDiv refactoring

--- components/common/styles/flexDiv.ts
+++ components/common/styles/flexDiv.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
+interface FlexDivInterface {
+    width?: string
+}
 
 export const FlexDiv = styled.div`
   display: flex;
+  ${(props: FlexDivInterface) => props.width? `width:${props.width};`:""};
 `;
