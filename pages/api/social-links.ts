c6af33b5664ddbb9d761561df2bd3eba2f18d6d2
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-16 20:32:58 +0100

    refactoring regarding new Icons

--- pages/api/social-links.ts
+++ pages/api/social-links.ts
@@ -1,31 +1,31 @@
 import type {NextApiRequest, NextApiResponse} from "next";
-import {SocialLink} from "@data/social-links";
+import {SocialIconInterface} from "@data/social-links";
 
-const handler = (req: NextApiRequest, res: NextApiResponse<SocialLink[]>) => {
+const handler = (req: NextApiRequest, res: NextApiResponse<SocialIconInterface[]>) => {
     res.status(200).json(
         [
             {
-                name: "Telegram",
+                title: "Telegram",
                 link: "https://t.me/AlinaSV",
                 action: "open"
             },
             {
-                name: "Linkedin",
+                title: "Linkedin",
                 link: "https://www.linkedin.com/in/oleksii-mukharovskyi-679795217/",
                 action: "open"
             },
             {
-                name: "Instagram",
+                title: "Instagram",
                 link: "https://www.instagram.com/alex_sumvar/",
                 action: "open"
             },
             {
-                name: "GitHub",
+                title: "GitHub",
                 link: "https://github.com/SumVur",
                 action: "open"
             },
             {
-                name: "Email",
+                title: "Email",
                 link: "mailto:a.mukharovskiy@gmail.com",
                 action: "location"
             }
