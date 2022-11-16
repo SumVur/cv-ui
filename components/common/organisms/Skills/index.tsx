2c8d41ffc6afed608a0e68c0a1be9e9f072b2075
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-16 22:30:16 +0100

    add with SkillDivision

--- components/common/organisms/Skills/index.tsx
+++ components/common/organisms/Skills/index.tsx
@@ -2,26 +2,22 @@ import {FC} from "react";
 import {FlexDiv} from "@components/common/styles"
 import useSWR from "swr";
 import fetch from "node-fetch";
-import {SkillInterface} from "@data/skill";
 import {HoverActionIconStyle} from "@common/styles/HoverActionIcon";
 import scss from "./styles/styles.module.scss"
-import {SkillIcon} from "@common/atoms";
+import {SkillDivision} from "@common/molecules";
 
 export interface SkillsStyles {
     skillIconStyle: HoverActionIconStyle;
 }
 
 const Skills: FC<SkillsStyles> = ({skillIconStyle}) => {
-    const {data} = useSWR('/api/skills', (apiURL: string) => fetch(apiURL, {timeout: 8000}).then(res => res.json()))
+    const {data} = useSWR('/api/skills/divisions', (apiURL: string) => fetch(apiURL, {timeout: 8000}).then(res => res.json()))
 
     return (
         <FlexDiv className={scss.Skills}>
-            {data ? data?.map((item: SkillInterface, index: string) => (<SkillIcon
-                key={index}
-                id={`${item.title}_skill`}
-                {...item}
-                {...skillIconStyle}
-            />)) : (
+            {data ? data?.map((item: string, index: string) => (
+                <SkillDivision key={index} title={item} skillIconStyle={skillIconStyle}/>
+            )) : (
                 <div>
                     loading...
                 </div>
