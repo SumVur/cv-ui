fded723c847eb166410adb743c124bd28d9b04e9
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-16 10:03:47 +0100

    Profile Main Info: refactoring

--- components/common/molecules/ProfileMainInfo/index.tsx
+++ components/common/molecules/ProfileMainInfo/index.tsx
@@ -2,12 +2,12 @@ import {ProfileImage} from "@common/atoms";
 import {FlexDiv} from "@common/styles"
 import style from "./styles/styles.module.scss";
 
-const ProfileMainInfo = ({}) => {
+const ProfileMainInfo = () => {
     return (
         <FlexDiv className={style.profileInfo}>
             <ProfileImage/>
             <FlexDiv className={style.about}>
-                <h1>Oleksii Mukharoskyi</h1>
+                <h1>Oleksii Mukharovskyi</h1>
                 <h2>Magento 2 Full Stack Developer</h2>
             </FlexDiv>
         </FlexDiv>
