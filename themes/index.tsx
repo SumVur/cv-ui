0e1690466f33e0f126f42a625b3773c4af1f935d
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-16 20:33:04 +0100

    refactoring regarding new Icons

--- themes/index.tsx
+++ themes/index.tsx
@@ -1,18 +1,34 @@
 import {MainLayoutStyles} from "@components/layout/MainLayout";
+import {SkillsPageStyles} from "../pages/skills";
 
 export interface Theme {
     mainLayout: MainLayoutStyles
+    skillsPage: SkillsPageStyles
 }
+
 const iconSize = 32;
 export const theme: Theme = {
     mainLayout: {
         headerStyle: {
             socialIconStyle: {
-                width:iconSize,
-                height:iconSize,
+                width: iconSize,
+                height: iconSize,
                 fill: ["#C3D0D5"],
                 hoverColor: "#F5BE17",
             }
+        }
+    },
+    skillsPage: {
+        skillIconStyle: {
+            width: 80,
+            height: 80,
+            fill: [
+                "#87aebe", "#7a9394", "#839da1",
+                "#748383", "#83989d", "#556569",
+                "#4e5b62",
+            ],
+            useDefaultColors:true,
+            hoverColor: "#F5BE17",
         }
     }
 }
