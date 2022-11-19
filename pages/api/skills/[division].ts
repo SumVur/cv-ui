410713fbe50dfb92150ec9d173228ec6d8f7aa76
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-19 13:46:39 +0100

    skills data refactoring

--- pages/api/skills/[division].ts
+++ pages/api/skills/[division].ts
@@ -1,126 +1,16 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 import {SkillInterface} from "@data/skill";
+import {getSkills} from "./index";
 
 interface DivisionsInterface {
     [title: string]: SkillInterface[]
 }
 
-const path = "/skills"
-
 export const divisions: DivisionsInterface = {
-    "Beck-end": [
-        {
-            title: "PHP",
-            link: `${path}`,
-            action: "location"
-        },
-        {
-            title: "JavaScript",
-            link: `${path}`,
-            action: "location"
-        },
-        {
-            title: "TypeScript",
-            link: `${path}`,
-            action: "location"
-        },
-        {
-            title: "NodeJs",
-            link: `${path}`,
-            action: "location"
-        },
-    ],
-    "Front-end": [
-        {
-            title: "HTML",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "CSS",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "JavaScript",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "TypeScript",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "SASS",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "ReactF",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "Jquery",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "RequireJs",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "Knockout",
-            link:`${path}`,
-            action:"location"
-        },
-    ],
-    "Databases-store":[
-        {
-            title: "MySql",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "Postgres",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "MongoDb",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "Redis",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "Elasticsearch",
-            link:`${path}`,
-            action:"location"
-        },
-    ],
-    "Other":[
-        {
-            title: "XDebug",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "Docker",
-            link:`${path}`,
-            action:"location"
-        },
-        {
-            title: "Kubernetes",
-            link:`${path}`,
-            action:"location"
-        }
-    ]
+    "Back-end": getSkills(["PHP", "JavaScript", "TypeScript", "NodeJs"]),
+    "Front-end": getSkills(["HTML", "CSS", "JavaScript", "TypeScript", "SASS", "ReactF", "Jquery", "RequireJs", "Knockout"]),
+    "Databases-store": getSkills(["MySql", "Postgres", "MongoDb", "Redis", "Elasticsearch"]),
+    "Other": getSkills(["XDebug", "Docker", "Kubernetes"])
 }
 const handler = (req: NextApiRequest, res: NextApiResponse<SkillInterface[]>) => {
     const {division} = req.query
--- pages/api/skills/index.ts
+++ pages/api/skills/index.ts
@@ -1,104 +1,116 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 import {SkillInterface} from "@data/skill";
+import * as icons from "@media/generated/Skills";
 
 const path = "/skills"
-const skills:SkillInterface[] = [
+const skills: SkillInterface[] = [
     {
         title: "PHP",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "HTML",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "CSS",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "JavaScript",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "TypeScript",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "SASS",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "NodeJs",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "ReactF",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "Jquery",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "RequireJs",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "Knockout",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "MongoDb",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "Redis",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "Elasticsearch",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "MySql",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "Postgres",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "XDebug",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "Docker",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     },
     {
         title: "Kubernetes",
-        link:`${path}`,
-        action:"location"
+        link: `${path}`,
+        action: "location"
     }
 ];
+
+export function getSkills(skillsTitles: (keyof typeof icons)[]): SkillInterface[] {
+    let finalSkills: SkillInterface[] = [];
+
+    skillsTitles.forEach((item) => {
+        finalSkills.push(skills.filter((skill) => skill.title === item)[0])
+    })
+
+    return finalSkills;
+}
+
 const handler = (req: NextApiRequest, res: NextApiResponse<SkillInterface[]>) => {
     res.status(200).json(skills);
 };
