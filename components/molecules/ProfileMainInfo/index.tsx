fb2e8e9b12311f180fb640cca2c29d1cc8d07722
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-20 12:11:31 +0100

    ProfileMainInfo: add navigation to home page

--- components/molecules/ProfileMainInfo/index.tsx
+++ components/molecules/ProfileMainInfo/index.tsx
@@ -1,10 +1,18 @@
 import {ProfileImage} from "@atoms";
 import {FlexDiv} from "@styles"
 import style from "./styles/styles.module.scss";
+import {useRouter} from "next/router";
 
 const ProfileMainInfo = () => {
+    const router = useRouter();
+
     return (
-        <FlexDiv className={style.profileInfo}>
+        <FlexDiv
+            className={style.profileInfo}
+            onClick={() => {
+                router.push("/")
+            }}
+        >
             <ProfileImage/>
             <FlexDiv className={style.about}>
                 <h1>Oleksii Mukharovskyi</h1>
