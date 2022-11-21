3b09bed2441a17c2a9f8dcd2ab46ff946419ac2a
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-21 21:01:25 +0100

    add SWRConfig include fetcher with token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,24 @@ import type {AppProps} from 'next/app'
 import {theme} from "themes";
 import {ThemeProvider} from "styled-components";
 import {appWithTranslation} from "next-i18next";
-import {MainLayout} from "@layout";
 import {Cardo} from '@next/font/google'
 import './styles.scss'
+import {MainLayout} from "@layout";
+import {createFetcher, loadToken} from "../helper";
+import {SWRConfig} from 'swr';
+import {useEffect, useState} from "react";
 
 const cardo = Cardo({weight: "400", subsets: ["latin"]})
 
 function MyApp({Component, pageProps}: AppProps) {
+    const [token, setToken] = useState(null)
+
+    useEffect(() => {
+        (async function anyNameFunction() {
+            const token = await loadToken();
+            setToken(token)
+        })();
+    }, []);
     return (
         <>
             <style jsx global>{`
@@ -16,11 +27,18 @@ function MyApp({Component, pageProps}: AppProps) {
                 font-family: ${cardo.style.fontFamily};
               }
             `}</style>
-
             <ThemeProvider theme={theme}>
-                <MainLayout>
-                    <Component {...pageProps} />
-                </MainLayout>
+                {token ? (
+                    <SWRConfig value={{fetcher: createFetcher(token)}}>
+                        <MainLayout>
+                            <Component {...pageProps} />
+                        </MainLayout>
+                    </SWRConfig>
+                ) : (
+                    <div>
+                        loading...
+                    </div>
+                )}
             </ThemeProvider>
         </>
     )
