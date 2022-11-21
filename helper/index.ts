8b6c86ce4f5546ff2422db04ca79b50a36a2a987
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-21 14:55:54 +0100

    add tokenHelper

---
+++ helper/index.ts
@@ -0,0 +1,27 @@
+import fetch from "node-fetch";
+
+export function createFetcher(token: string): (url: string) => Promise<any> {
+    return (url: string,) => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'Authorization': `${token}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+        }
+        return fetch(url, options).then(res => res.json())
+    }
+}
+
+export function getToken(): string {
+    return `${process.env.AUTH_TOKEN}`
+}
+
+export async function loadToken() {
+    const res = await fetch(`${window.location.origin}/api/getToken`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+
+    return res.json();
+}
