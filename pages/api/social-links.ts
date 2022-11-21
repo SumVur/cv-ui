41c535dafc419ae5b7231b76c302ce4c4c7d7b86
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-21 23:18:30 +0100

    rewrite with supabase

--- pages/api/social-links.ts
+++ pages/api/social-links.ts
@@ -1,36 +1,16 @@
 import type {NextApiRequest, NextApiResponse} from "next";
 import {SocialIconInterface} from "@data/social-links";
+import {supabase} from "@helpers";
 
-const handler = (req: NextApiRequest, res: NextApiResponse<SocialIconInterface[]>) => {
-    res.status(200).json(
-        [
-            {
-                title: "Telegram",
-                link: "https://t.me/AlinaSV",
-                action: "open"
-            },
-            {
-                title: "Linkedin",
-                link: "https://www.linkedin.com/in/oleksii-mukharovskyi-679795217/",
-                action: "open"
-            },
-            {
-                title: "Instagram",
-                link: "https://www.instagram.com/alex_sumvar/",
-                action: "open"
-            },
-            {
-                title: "GitHub",
-                link: "https://github.com/SumVur",
-                action: "open"
-            },
-            {
-                title: "Email",
-                link: "mailto:a.mukharovskiy@gmail.com",
-                action: "location"
-            }
-        ]
-    );
+const handler = async (req: NextApiRequest, res: NextApiResponse<SocialIconInterface[]>) => {
+    const {data, error} = await supabase
+        .from("social-links")
+        .select("title,link,action");
+    if (error) {
+        res.status(500);
+        return;
+    }
+    res.status(200).json(data);
 };
 
 export default handler;
