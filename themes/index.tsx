e190cf6772dd8ab2a744ef1ede21877de64820c5
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-11-22 23:30:11 +0100

    add theme for projects

--- themes/index.tsx
+++ themes/index.tsx
@@ -1,9 +1,11 @@
 import {MainLayoutStyles} from "@components/layout/MainLayout";
 import {SkillsPageStyles} from "../pages/skills";
+import {ExperienceStyles} from "../pages/experience";
 
 export interface Theme {
     mainLayout: MainLayoutStyles
     skillsPage: SkillsPageStyles
+    experiencePage: ExperienceStyles
 }
 
 const iconSize = 32;
@@ -33,6 +35,21 @@ export const theme: Theme = {
             },
             titleStyle: {
                 color: "black"
+            }
+        }
+    },
+    experiencePage: {
+        projectStyle: {
+            skillIconStyle: {
+                width: 40,
+                height: 40,
+                fill: [
+                    "#c5ced7", "#9ab5b6", "#839da1",
+                    "#748383", "#83989d", "#556569",
+                    "#4e5b62",
+                ],
+                useDefaultColors: true,
+                hoverColor: "#F5BE17",
             }
         }
     }
