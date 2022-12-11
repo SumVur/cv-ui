b04ec22c03782bdcbc13ba32bc7362d4e594cf1d
Oleksii Mukahrovskyi <a.mukharovskiy@gmail.com>
2022-12-11 20:43:45 +0100

    rewrite with apollo client

--- components/molecules/SkillDivision/index.tsx
+++ components/molecules/SkillDivision/index.tsx
@@ -1,36 +1,47 @@
 import {FC} from "react";
-import useSWR from "swr";
 import {SkillIcon} from "@atoms";
 import {FlexDiv, HoverActionIconStyle, TextInterface, H3} from "@styles"
-import {SkillInterface} from "@data/skill";
 import scss from "./styles/styles.module.scss"
+import {GetAllSkillsInDivisionQuery, SkillsDivision} from "@graphql/generated/graphql";
+import {useQuery} from "@apollo/client";
+import GetAllSkillsInDivision from "@graphql/queries/GetAllSkillsInDivision.graphql";
 
 export interface SkillDivisionStyles {
     skillIconStyle: HoverActionIconStyle;
     titleStyle: TextInterface
 }
 
-type SkillDivisionProps = SkillDivisionStyles & { title: string }
+type SkillDivisionProps = SkillDivisionStyles & Pick<SkillsDivision, "title" | "id">
 
-const SkillDivision: FC<SkillDivisionProps> = ({title, skillIconStyle, titleStyle}) => {
-    const {data} = useSWR(`/api/skills/${title}`)
+const SkillDivision: FC<SkillDivisionProps> = ({title, id, skillIconStyle, titleStyle}) => {
+    const {data, loading, error} = useQuery<GetAllSkillsInDivisionQuery>(GetAllSkillsInDivision, {variables: {id: id}});
+
+    if (error) {
+        return null;
+    }
+
+    const items = data?.getSkillsDivision?.skills?.map((item, index) => (
+        <SkillIcon
+            key={index}
+            actionIconClassName={scss.Icon}
+            id={`${item.title}_skill`}
+            link={item.link}
+            // @ts-ignore
+            title={item.title}
+            {...skillIconStyle}
+        />
+    ))
 
     return (
         <FlexDiv className={scss.SkillsWrapper}>
             <H3 {...titleStyle}>{title}</H3>
             <hr className={scss.hr}/>
             <FlexDiv className={scss.Skills}>
-                {data ? data?.map((item: SkillInterface, index: string) => (<SkillIcon
-                    actionIconClassName={scss.Icon}
-                    key={index}
-                    id={`${item.title}_skill`}
-                    {...item}
-                    {...skillIconStyle}
-                />)) : (
+                {loading ? (
                     <div>
                         loading...
                     </div>
-                )}
+                ) : items}
             </FlexDiv>
         </FlexDiv>
     )
--- components/organisms/Skills/index.tsx
+++ components/organisms/Skills/index.tsx
@@ -1,25 +1,32 @@
 import {FC} from "react";
-import useSWR from "swr";
+import {useQuery} from "@apollo/client";
 import {FlexDiv} from "@styles";
 import {SkillDivision, SkillDivisionStyles} from "@molecules";
 import scss from "./styles/styles.module.scss"
+import GetAllSkillsDivisionsTitle from "@graphql/queries/GetAllSkillsDivisionsTitle.graphql";
+import {GetAllSkillsDivisionsTitleQuery} from "@graphql/generated/graphql";
 
 export interface SkillsStyles {
     skillDivisionStyle: SkillDivisionStyles
 }
 
 const Skills: FC<SkillsStyles> = ({skillDivisionStyle}) => {
-    const {data} = useSWR('/api/skills/divisions')
+    const {data, loading, error} = useQuery<GetAllSkillsDivisionsTitleQuery>(GetAllSkillsDivisionsTitle);
+
+    if (error) {
+        return null;
+    }
+    const SkillDivisions = data?.getSkillsDivisions?.map((item, index) => (
+        <SkillDivision key={index} id={item.id} title={item.title} {...skillDivisionStyle}/>
+    ))
 
     return (
         <FlexDiv className={scss.Skills}>
-            {data ? data?.map((item: string, index: string) => (
-                <SkillDivision key={index} title={item} {...skillDivisionStyle}/>
-            )) : (
+            {loading ? (
                 <div>
                     loading...
                 </div>
-            )}
+            ) : SkillDivisions}
         </FlexDiv>
     )
 }
